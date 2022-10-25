622dbdd2076eeefa0be13ea3a4d20e888f3c94d0
Matteo <matteo.baux@epsi.fr>
2022-10-25 13:21:04 +0200

    Ajout fonction read et export

--- TriCsv.py
+++ TriCsv.py
@@ -1,8 +1,19 @@
 import pandas as pd
 import numpy as np
 
-df1 = pd.read_csv('', delimiter= ';')
+df1 = pd.read_csv('conso-annuelles_v1.csv', delimiter= ';')
 
 csvFile = np.array(df1)
 
 df_
+
+triCsv = []
+
+for dataCsv in csvFile:
+    dataFound = False
+    if dataCsv
+
+with open('conso-clean.csv', 'w') as file:
+    for dataFound in triCsv :
+        file.write(dataFound[])
+
