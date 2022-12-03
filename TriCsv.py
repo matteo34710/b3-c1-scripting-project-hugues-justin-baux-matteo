1c90ec752d212afb7f396ce9ffb2c9bb96fa829c
Matteo <matteo.baux@epsi.fr>
2022-12-04 00:59:02 +0100

    Suppression des lignes avec colonnes vides et suppression colonne id logement (Justin)

--- TriCsv.py
+++ TriCsv.py
@@ -4,3 +4,17 @@ print("Tri commencé!")
 #ouverture du fichier qui doit être trié
 file = open('conso-annuelles_v1.csv', 'r')
 csvFile = csv.reader(file, delimiter=';')
+
+finalList = []
+calculList = []
+sortList = []
+
+#Suppression des lignes avec une colonne vide
+for line in csvFile:
+    if all(line):
+        calculList.append(line)
+
+#Suppression de la colonne id logement
+for col in calculList:
+    del col[1]
+
